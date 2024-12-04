eb0e6cab1fcee331d14f27636125d5cd0c69df75
HarutoNakamura <nahanaha328@gmail.com>
2024-12-04 21:21:15 +0900

    SSL

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ type Post = { id: number; email: string; content: string; created_at: string; li
 type Reply = { email: string; content: string; created_at: string };
 
 const API_BASE_URL = "https://hackathon-back-297164197657.us-central1.run.app";
-//const API_BASE_URL = "https://localhost:8081";
+//const API_BASE_URL = "https://localhost:8080";
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
